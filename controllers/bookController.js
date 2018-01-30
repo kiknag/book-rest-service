597be7074ff78349677e0c845f182bfd38e0d49f
George Kiknadze <g.kiknadze@hotmail.com>
2018-01-31 00:31:44 +0400

    write down tasks for book routes. refactor names.

---
+++ controllers/bookController.js
@@ -0,0 +1,9 @@
+// Book listing page
+exports.addBook = (req, res) => {
+  res.render('index', { title: 'Listing page' });
+};
+
+// TODO: Certain book route (GET)
+// TODO: Add new book route (POST)
+// TODO: Delete existing book (GET/DELETE)
+// TODO: Update existing book (POST/PUT/UPDATE) 
--- routes/book.js
+++ routes/book.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
-router.get('/', bookController.listing);
+router.post('/addbook', bookController.addBook);
 
 module.exports = router;
